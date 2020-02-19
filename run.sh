fa337ad99d4d43ec12a1179067b5e91372937b33
Sophia Vinci-Booher <svincibo@gmail.com>
2020-02-19 17:26:06 -0500

    edited run.sh

--- run.sh
+++ run.sh
@@ -8,10 +8,9 @@ t2=$(jq -r .t2 config.json)
 #atlas=$(jq -r .atlas config.json)
 
 # Coregister t2 to t1
-flirt -in $t2 -ref $t1 -dof 6 -cost leastsq -out t2_aligned_to_t1 
-
+singularity exec -e docker://brainlife/fsl:5.0.9 flirt -in $t2 -ref $t1 -dof 6 -cost leastsq -out output/ashs/t2_aligned_to_t1 
 
 mkdir -p output/ashs
 
-ashs_main.sh -a /atlas -g $t1 -f t2_aligned_to_t1.nii.gz -w output/ashs
+ashs_main.sh -a /atlas -g $t1 -f output/ashs/t2_aligned_to_t1.nii.gz -w output/ashs
 cp /atlas/snap/snaplabels.txt output/ashs

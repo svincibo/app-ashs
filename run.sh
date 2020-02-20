12de62bc99b13bb3fea5cfa47165e7f06aa8b3d9
Sophia Vinci-Booher <svincibo@gmail.com>
2020-02-20 08:08:50 -0500

    updated main and run

--- run.sh
+++ run.sh
@@ -3,14 +3,12 @@
 set -e
 set -x
 
-t1=$(jq -r .t1 config.json)
-t2=$(jq -r .t2 config.json)
+#t1=$(jq -r .t1 config.json)
+#t2=$(jq -r .t2 config.json)
 #atlas=$(jq -r .atlas config.json)
 
-# Coregister t2 to t1
-singularity exec -e docker://brainlife/fsl:5.0.9 flirt -in $t2 -ref $t1 -dof 6 -cost leastsq -out output/ashs/t2_aligned_to_t1 
+# Run the main ashs segmentation script.
+ashs_main.sh -a /atlas -g $1 -f $2 -w output/ashs
 
-mkdir -p output/ashs
-
-ashs_main.sh -a /atlas -g $t1 -f output/ashs/t2_aligned_to_t1.nii.gz -w output/ashs
+# Copy snaplabels to the output so that the labels are with the raw ashs output if the user wants to download and work with it outside of brainlife.
 cp /atlas/snap/snaplabels.txt output/ashs

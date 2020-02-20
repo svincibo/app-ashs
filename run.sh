1873f6c12e53d10c19ad5dae92615a1723358607
Sophia Vinci-Booher <svincibo@gmail.com>
2020-02-20 09:52:59 -0500

    fixed bug

--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@ set -x
 #atlas=$(jq -r .atlas config.json)
 
 # Run the main ashs segmentation script.
-ashs_main.sh -a /atlas -g $1 -f $2 -w output/ashs
+ashs_main.sh -a /atlas -g $1 -f ${2}.nii.gz -w output/ashs
 
 # Copy snaplabels to the output so that the labels are with the raw ashs output if the user wants to download and work with it outside of brainlife.
 cp /atlas/snap/snaplabels.txt output/ashs

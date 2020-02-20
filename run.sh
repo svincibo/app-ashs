be10b5149adb2b0710ebb476ad77ca36dfbba476
Sophia Vinci-Booher <svincibo@gmail.com>
2020-02-20 14:37:23 -0500

    minor edit

--- run.sh
+++ run.sh
@@ -8,10 +8,10 @@ set -x
 #atlas=$(jq -r .atlas config.json)
 
 # Verify that the T2 slices are in the z-direction. Just show the user, for now.
-#$ASHS_ROOT/ext/Linux/bin/c3d $t2 -info
+$ASHS_ROOT/ext/Linux/bin/c3d $2 -info
 
 # Run the main ashs segmentation script.
-ashs_main.sh -a /atlas -g $1 -f ${2}.nii.gz -w output/ashs
+ashs_main.sh -a /atlas -g $1 -f $2 -w output/ashs
 
 # Copy snaplabels to the output so that the labels are with the raw ashs output if the user wants to download and work with it outside of brainlife.
 cp /atlas/snap/snaplabels.txt output/ashs

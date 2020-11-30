5976e6230a30ccc4a5a2dd35b0072aae9595e0cb
svincibo <svincibooher@gmail.com>
2020-11-30 15:42:31 -0500

    edits to main

--- run.sh
+++ run.sh
@@ -12,6 +12,7 @@ $ASHS_ROOT/ext/Linux/bin/c3d $2 -info
 
 # Run the main ashs segmentation script.
 ashs_main.sh -a /atlas -g $1 -f $2 -w output/ashs
+#singularity exec docker://brainlife/ashs:2.0.0 /ashs/bin/ashs_main.sh -a /atlas -g $1 -f $2 -w output/ashs
 
 # Copy snaplabels to the output so that the labels are with the raw ashs output if the user wants to download and work with it outside of brainlife.
 cp /atlas/snap/snaplabels.txt output/ashs
